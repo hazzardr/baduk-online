8e1850a928dcbae86e8c9f5033cdbcc675065696
brian <rbrianhazzard@gmail.com>
2025-07-23 21:54:41 -0400

    updates

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,17 +1,28 @@
 package api
 
 import (
+	"context"
 	"log/slog"
 	"net/http"
 )
 
 func (api *API) HealthCheckHandler(w http.ResponseWriter, _ *http.Request) {
-	hc := map[string]string{
-		"status":  "OK",
+	statuses := make(map[string]string)
+	err := api.db.Ping(context.Background())
+
+	if err != nil {
+		slog.Error("db conn failed", "err", err)
+		statuses["db"] = "DOWN"
+	} else {
+		statuses["db"] = "OK"
+	}
+
+	hc := map[string]any{
+		"status":  statuses,
 		"env":     api.environment,
 		"version": api.version,
 	}
-	err := api.writeJSON(w, http.StatusOK, hc, nil)
+	err = api.writeJSON(w, http.StatusOK, hc, nil)
 	if err != nil {
 		slog.Error("failed to marshal health check response", slog.Any("error", err))
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
