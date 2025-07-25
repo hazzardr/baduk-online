26d25612cc3edc6cfd9055cfbf4b51745b0bb6bd
brian <rbrianhazzard@gmail.com>
2025-07-25 18:50:38 -0400

    fix extensions

--- migrations/20250719222618_users.sql
+++ migrations/20250719222618_users.sql
@@ -1,4 +1,7 @@
 -- +goose Up
+CREATE EXTENSION IF NOT EXISTS citext;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE IF NOT EXISTS users (
     id bigserial PRIMARY KEY,
     created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
@@ -12,3 +15,5 @@ CREATE TABLE IF NOT EXISTS users (
 
 -- +goose Down
 DROP TABLE IF EXISTS users;
+DROP EXTENSION IF EXISTS "uuid-ossp"
+DROP EXTENSION IF EXISTS citext;
